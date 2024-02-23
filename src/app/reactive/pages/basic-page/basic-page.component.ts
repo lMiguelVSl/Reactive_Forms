0296bfe1821a48e391862924ee75bd3b3fd205fd
Miguel Vargas <miguel.vargas.work@outlook.com>
2024-02-22 20:44:56 -0500

    application structure. components page

---
+++ src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  standalone: true,
+  imports: [],
+  templateUrl: './basic-page.component.html',
+  styles: ``
+})
+export class BasicPageComponent {
+
+}
---
+++ src/app/reactive/pages/switches-page/switches-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  standalone: true,
+  imports: [],
+  templateUrl: './switches-page.component.html',
+  styles: ``
+})
+export class SwitchesPageComponent {
+
+}
